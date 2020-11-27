3774d341abae3a84d2a8f7a77763207f43b2d8fd
Henry Kimura <htkimura@gmail.com>
2020-11-27 14:17:21 -0300

    cleaning code

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,6 @@ import Home from './pages/Home'
 import AppContext, { AppContextDefaultValue } from './store'
 
 import AppStyled from './styles/App.styled'
-import BackgroundContainerStyled from './styles/Background'
 import EmailVerify from './pages/EmailVerify'
 
 function App() {
@@ -17,11 +16,9 @@ function App() {
 			<AppStyled className="App">
 				<Router>
 					<Switch>
-						{/* <BackgroundContainerStyled> */}
 						<Route exact component={Home} path={HOME} />
 						<PrivateRoute exact component={EmailVerify} path={EMAILVERIFY} />
 						<Route component={() => <Redirect to={HOME} />} />
-						{/* </BackgroundContainerStyled> */}
 					</Switch>
 				</Router>
 			</AppStyled>
